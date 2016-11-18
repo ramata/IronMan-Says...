538b3f81831cc91e87251349fe00722c80e2b495
Ramata Diallo <rama.diallo@gmail.com>
2016-11-18 10:18:22 -0500

    added background image

--- script.js
+++ script.js
@@ -21,8 +21,8 @@ var soundAdd = ['a_sharp.mp3',
 
 var gameOn = false;
 
-//Store Simon's sequence of actions in this array
-var simonSequence = [];
+//Store Iron Man's sequence of actions in this array
+var ironManSequence = [];
 var userSequence = [];
 
 function getRand() {
@@ -31,29 +31,26 @@ function getRand() {
 
 function newMove() {
     var par = getRand();
-    simonSequence.push(par - 1);
+    ironManSequence.push(par - 1);
 }
 
 var audio = new Audio();
 
 function playAudio(index) {
-  console.log("this ran");
     audio.src = (soundAdd[index]);
     audio.play();
 }
 
 function darkenPar(index) {
-  console.log("Hey from darkenPar")
-  $("parts").addClass('played');
     parts[index].className += ' played';
     setTimeout(function() {
         parts[index].classList.remove('played');
     }, 500);
 }
 
-function simonSays(i) {
+function ironManSays(i) {
     setTimeout(function() {
-        var parIndex = simonSequence[i];
+        var parIndex = ironManSequence[i];
         playAudio(parIndex);
         darkenPar(parIndex);
         userSequence = []; //clears userSeq so user has to click all parts again
@@ -63,7 +60,7 @@ function simonSays(i) {
 function compareSequences() {
     console.log('compared');
     for (var i = 0; i < userSequence.length; i += 1) {
-        if (simonSequence[i] !== userSequence[i]) {
+        if (ironManSequence[i] !== userSequence[i]) {
             return false;
         }
     }
@@ -76,7 +73,7 @@ function updateCount(inc) {
 }
 
 function reset() {
-    simonSequence = [];
+    ironManSequence = [];
     userSequence = [];
     gameStarted = false;
     correctCount = 0;
@@ -89,71 +86,70 @@ function retry() {
     endScreen.classList.remove('hidden');
     setTimeout(function() {
         endScreen.classList.add('hidden');
-        for (var i = 0; i < simonSequence.length; i += 1) {
-            simonSays(i);
+        for (var i = 0; i < ironManSequence.length; i += 1) {
+            ironManSays(i);
         }
     }, 1200);
 }
 
 function makePartsClickable(par, index) {
     par.addEventListener('click', function() {
-                if (userSequence.length < simonSequence.length) {
-                    userSequence.push(index);
-                    playAudio(index);
-                    darkenPar(index);
-                    //compare partial sequence to simonsequence
-                }
-            });
+        if (userSequence.length < ironManSequence.length) {
+            userSequence.push(index);
+            playAudio(index);
+            darkenPar(index);
+            //compare partial sequence to ironMan sequence
         }
-        //compare sequences  //todo make function
-    var compare = compareSequences();
-    if (compare === false) {
+    });
+}
+//compare sequences  //todo make function
+var compare = compareSequences();
+if (compare === false) {
         endScreenMessage.innerHTML = 'Lose';
         endScreen.classList.remove('hidden');
         setTimeout(function() {
             endScreen.classList.add('hidden');
-        })
-    } else {
-        retry();
-
-    }
-    if (userSequence.length === simonSequence.length) {
-            updateCount(1);
-            setTimeout(simonTurn, 1500);
-        }
-
-    if (correctCount === 10) {
-        endScreen.classList.remove('hidden');
-        endScreenMessage.innerHTML = 'Nice Job!';
-        gameStarted = false;
-        setTimeout(function() {
-            endScreen.classList.add('hidden');
-            gameStarted = true;
-            simonTurn();
-        }, 5000);
-    }
-
-
-    for (var i = 0; i < 4; i += 1) {
-        makePartsClickable(parts[i], i);
-    }
-
-    function simonTurn() {
-        if (gameStarted === true) {
-            newMove(); //todo only add new move after verrifying that userSequence matches simonSequence
-            for (var i = 0; i < simonSequence.length; i += 1) {
-                simonSays(i);
-            }
-        }
-    }
-
-    startBtn.addEventListener('click', function() {
-        if (gameStarted === false) {
-            gameStarted = true;
-            setTimeout(simonTurn, 1000);
-        }
     });
+ } else {
+    retry();
+}
+    if (userSequence.length === ironManSequence.length) {
+        updateCount(1);
+        setTimeout(ironManTurn, 1500);
+    }
 
-    resetBtn.addEventListener('click', function() {
-        reset();
-    });
+if (correctCount === 10) {
+    endScreen.classList.remove('hidden');
+    endScreenMessage.innerHTML = 'Nice Job!';
+    gameStarted = false;
+    setTimeout(function() {
+        endScreen.classList.add('hidden');
+        gameStarted = true;
+        ironManTurn();
+    }, 5000);
+}
+
+
+for (var i = 0; i < 4; i += 1) {
+    makePartsClickable(parts[i], i);
+}
+
+function ironManTurn() {
+    if (gameStarted === true) {
+        newMove(); //todo only add new move after verrifying that userSequence matches ironManSequence
+        for (var i = 0; i < ironManSequence.length; i += 1) {
+            ironManSays(i);
+        }
+    }
+}
+
+startBtn.addEventListener('click', function() {
+    if (gameStarted === false) {
+        gameStarted = true;
+        setTimeout(ironManTurn, 1000);
+    }
+});
+
+resetBtn.addEventListener('click', function() {
+    reset();
+});
